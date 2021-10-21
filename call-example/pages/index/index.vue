9c2b9398ebe0cb66cddd5ad35204fa05760010b5
lijiakuo <lijiakuo1@rongcloud.cn>
2021-10-21 10:08:49 +0800

    fix: 更新代码

--- call-example/pages/index/index.vue
+++ call-example/pages/index/index.vue
@@ -148,8 +148,8 @@
 				isLogining: "",
 				isInitIm: false,
 				form:{
-					appkey:'z3v5yqkbv8v30',
-					token:'Y5w0rF/AzBn5zi3SVx8RkZU2+79mjpXMq7+si4yGEz4CjTHnlb2xVQ==@emx6.cn.rongnav.com;emx6.cn.rongcfg.com',
+					appkey:'',
+					token:'',
 					navi:'',
 					mediaServer:''
 				},
@@ -180,10 +180,10 @@
 						label:'音视频'
 					}
 				],
-				targetId:'ftd_1629253053617',
+				targetId:'',
 				isRoom:false,
-				groupId:'pmd001',
-				userIds:'ftd_1629253053617',
+				groupId:'',
+				userIds:'',
 				isCut:false,
 				localSession:'',
 				showMask:false
